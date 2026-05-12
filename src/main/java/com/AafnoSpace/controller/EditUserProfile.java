f4c6df9373bd114622639646e7412862b86a6670
Sulav306 <sulavshrestha315@proton.me>
2026-05-12 19:49:37 +0545

    Adding error handling for edit profile for user

--- src/main/java/com/AafnoSpace/controller/EditUserProfile.java
+++ src/main/java/com/AafnoSpace/controller/EditUserProfile.java
@@ -5,7 +5,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+import java.io.File;
 import java.io.IOException;
+
+import com.AafnoSpace.utils.FileUploadUtil;
+import com.AafnoSpace.utils.SessionUtil;
 
 /**
  * Servlet implementation class EditUserProfile
@@ -13,7 +19,7 @@ import java.io.IOException;
 @WebServlet(asyncSupported = true, urlPatterns = { "/editUserProfile" })
 public class EditUserProfile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static final String UPLOAD_DIR = System.getProperty("user.home") + File.separator + "profilepicture_uploads";   
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,8 +40,24 @@ public class EditUserProfile extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		  Part filePart = request.getPart("profileImage");
+	        if (filePart != null && filePart.getSize() > 0) {
+	            if (FileUploadUtil.isImage(filePart)) {
+	                String extension = FileUploadUtil.getFileExtension(filePart.getSubmittedFileName());
+	                String fileName = "admin" + extension;
+	                FileUploadUtil.saveFile(filePart, UPLOAD_DIR, fileName);
+	                //if upload is successful
+	                response.sendRedirect(request.getContextPath() + "/AdminProfile");
+	            } 
+	            //if upload is unsuccessful
+	            else {
+	                SessionUtil.setAttribute(request, "error", "Invalid image type.", 60);
+	                response.sendRedirect(request.getContextPath() + "/editAdminProfile");
+	            }
+	        }
+	        else {
+	                response.sendRedirect(request.getContextPath() + "/AdminProfile");
+	            }
+	        }
 
 }
