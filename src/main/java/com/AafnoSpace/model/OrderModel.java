5c6c0cc4f97d3f3aeb8f4ebae7e23ae1bea205cb
unknown <aakipraj.77@gmail.com>
2026-05-14 12:40:24 +0545

    Updates in Order Model

--- src/main/java/com/AafnoSpace/model/OrderModel.java
+++ src/main/java/com/AafnoSpace/model/OrderModel.java
@@ -16,25 +16,15 @@ public class OrderModel {
 	        this.paymentId = paymentId;
 	    }
 
-	    public int getOrderId() {
-	        return orderId;
-	    }
+	    public int getOrderId() {return orderId;}
 
-	    public void setOrderId(int orderId) {
-	        this.orderId = orderId;
-	    }
+	    public void setOrderId(int orderId) {this.orderId = orderId;}
 
-	    public int getUserId() {
-	        return userId;
-	    }
+	    public int getUserId() {return userId;}
 
-	    public void setUserId(int userId) {
-	        this.userId = userId;
-	    }
+	    public void setUserId(int userId) {this.userId = userId;}
 
-	    public Date getOrderDate() {
-	        return orderDate;
-	    }
+	    public Date getOrderDate() {return orderDate;}
 
 	    public void setOrderDate(Date orderDate) {
 	        this.orderDate = orderDate;
